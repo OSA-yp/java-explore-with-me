edf604c387060da2cad3ace7c5bbece4ad1c8cb5
Bleachweb <Bleachweb123@yandex.ru>
2025-07-07 18:00:27 +0300

    Модуль клиента + dto

---
+++ stats-server/client/src/main/java/ru/practicum/StatsClient.java
@@ -0,0 +1,53 @@
+package ru.practicum;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class StatsClient {
+    private static final String HIT_ENDPOINT = "/hit";
+    private static final String STATS_ENDPOINT = "/stats";
+
+    private final RestClient restClient;
+
+    public StatsClient(@Value("${stats-service.url}") String statsServiceUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(statsServiceUrl)
+                .build();
+    }
+
+    public void sendHit(EndpointHitDto hit) {
+        restClient.post()
+                .uri(HIT_ENDPOINT)
+                .body(hit)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public List<ViewStats> getStats(String start, String end, List<String> uris, Boolean unique) {
+        if (start == null || end == null) {
+            throw new IllegalArgumentException("Дата и время начала и окончания диапазона должны быть указаны");
+        }
+
+        return restClient.get()
+                .uri(uriBuilder -> {
+                    uriBuilder.path(STATS_ENDPOINT)
+                            .queryParam("start", start)
+                            .queryParam("end", end);
+
+                    if (uris != null && !uris.isEmpty()) {
+                        uriBuilder.queryParam("uris", String.join(",", uris));
+                    }
+                    if (unique != null) {
+                        uriBuilder.queryParam("unique", unique);
+                    }
+                    return uriBuilder.build();
+                })
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
+}
---
+++ stats-server/common-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -0,0 +1,19 @@
+package ru.practicum;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
