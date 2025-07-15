ca1b2e0f05aaa8d92f3b0d9fb3a48df20504ee19
Bleachweb <Bleachweb123@yandex.ru>
2025-07-15 17:34:57 +0300

    лишний импорт убрал

--- ewm-service/src/main/java/ru/practicum/explore/server/event/service/PrivateEventService.java
+++ ewm-service/src/main/java/ru/practicum/explore/server/event/service/PrivateEventService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.practicum.StatsClient;
 import ru.practicum.explore.dto.ViewStatsDto;
